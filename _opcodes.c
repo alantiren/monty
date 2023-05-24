0dba1a8a220be1551466f9a97112744f0862bc30
alantiren <tirenalan0@gmail.com>
2023-05-24 20:10:07 +0300

    updates

--- _opcodes.c
+++ _opcodes.c
@@ -80,3 +80,15 @@ exit(EXIT_FAILURE);
 (*stack)->next->n += (*stack)->n;
 pop(stack, line_number);
 }
+
+/**
+ * nop - Does nothing
+ * @stack: Double pointer to the top of the stack
+ * @line_number: Line number of the opcode in the file
+ */
+
+void nop(stack_t **stack, unsigned int line_number)
+{
+(void)stack;
+(void)line_number;
+}
