c966c3901e71ff7ba9a497efdbe4dbef170f20f9
alantiren <tirenalan0@gmail.com>
2023-05-24 19:54:22 +0300

    updates

--- _opcodes.c
+++ _opcodes.c
@@ -27,14 +27,15 @@ printf("%d\n", (*stack)->n);
  */
 
 void pop(stack_t **stack, unsigned int line_number)
+void pop(stack_t **stack, unsigned int line_number)
 {
+stack_t *temp;
 if (*stack == NULL)
 {
 fprintf(stderr, "L%u: can't pop an empty stack\n", line_number);
 free_stack(*stack);
 exit(EXIT_FAILURE);
 }
-stack_t *temp;
 temp = *stack;
 *stack = (*stack)->next;
 if (*stack != NULL)
