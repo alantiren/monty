7b364032de74f36966a594edddde21db34a6fbe4
alantiren <tirenalan0@gmail.com>
2023-05-25 02:15:02 +0300

    updates

--- free_stack.c
+++ free_stack.c
@@ -11,16 +11,19 @@
  * pointers accordingly.
  */
 
-void free_stack(stack_t *stack)
+void free_stack(stack_t **stack)
 {
-stack_t *current = stack;
+stack_t *current = *stack;
 stack_t *next;
+
 while (current != NULL)
 {
 next = current->next;
 free(current);
 current = next;
 }
+
+*stack = NULL;
 }
 
 /**
