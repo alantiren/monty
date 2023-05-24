8baff5b4e36bd773ce8aba733f3818f89abf94b8
alantiren <tirenalan0@gmail.com>
2023-05-25 01:38:12 +0300

    updates

--- free_stack.c
+++ free_stack.c
@@ -22,3 +22,28 @@ free(current);
 current = next;
 }
 }
+
+/**
+ * pop - Removes the top element from the stack
+ * @stack: Double pointer to the top of the stack
+ * @line_number: Line number of the opcode in the file
+ *
+ * Description: The pop function removes the top element from the stack,
+ * updates the stack pointer, and frees the memory allocated for the removed element.
+ * If the stack is empty, an error message is printed and the program exits with EXIT_FAILURE.
+ */
+void pop(stack_t **stack, unsigned int line_number)
+{
+stack_t *temp;
+
+if (*stack == NULL)
+{
+fprintf(stderr, "L%u: can't pop from an empty stack\n", line_number);
+exit(EXIT_FAILURE);
+}
+temp = *stack;
+*stack = (*stack)->next;
+if (*stack != NULL)
+(*stack)->prev = NULL;
+free(temp);
+}
--- monty.h
+++ monty.h
@@ -56,6 +56,7 @@ void pstr(stack_t **, unsigned int line_number );
 void rotl(stack_t **stack);
 void rotr(stack_t **stack);
 void set_mode(stack_t **stack, int stack_mode_val, int queue_mode_val);
+void pop(stack_t **stack, unsigned int line_number);
 
 /* Other function prototypes... */
 extern int queue_mode;
