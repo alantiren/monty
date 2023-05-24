0737bf9c770697039d37d49f4fd4862cc136a6f6
alantiren <tirenalan0@gmail.com>
2023-05-24 20:42:50 +0300

    UPDATES

--- _opcodes.c
+++ _opcodes.c
@@ -112,3 +112,34 @@ result = (*stack)->next->n - (*stack)->n;
 pop(stack, line_number);
 (*stack)->n = result;
 }
+
+
+/**
+ * div_op - Divides the second top element of the stack by the top element
+ * @stack: Double pointer to the top of the stack
+ * @line_number: Line number of the opcode in the file
+ *
+ * Description: The div opcode divides the second top element of the stack
+ * by the top element of the stack. The result is stored in the second top
+ * element, and the top element is removed. If the stack contains less than
+ * two elements, or if the top element is 0, an error message is printed
+ * and the program exits with EXIT_FAILURE status.
+ */
+
+void div_op(stack_t **stack, unsigned int line_number)
+{
+if (*stack == NULL || (*stack)->next == NULL)
+{
+fprintf(stderr, "L%u: can't div, stack too short\n", line_number);
+exit(EXIT_FAILURE);
+}
+
+if ((*stack)->n == 0)
+{
+fprintf(stderr, "L%u: division by zero\n", line_number);
+exit(EXIT_FAILURE);
+}
+
+(*stack)->next->n /= (*stack)->n;
+pop(stack, line_number);
+}
