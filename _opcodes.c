1f34db09119fb3d00399b98de309d4d48910eddd
alantiren <tirenalan0@gmail.com>
2023-05-24 20:32:30 +0300

    updates

--- _opcodes.c
+++ _opcodes.c
@@ -101,12 +101,13 @@ void nop(stack_t **stack, unsigned int line_number)
 
 void sub(stack_t **stack, unsigned int line_number)
 {
+int result;
+
 if (*stack == NULL || (*stack)->next == NULL)
 {
 fprintf(stderr, "L%u: can't sub, stack too short\n", line_number);
 exit(EXIT_FAILURE);
 }
-
 result = (*stack)->next->n - (*stack)->n;
 pop(stack, line_number);
 (*stack)->n = result;
