d0858e1ca54c73e7d4591bf00de36dc90b70b850
alantiren <tirenalan0@gmail.com>
2023-05-25 01:41:57 +0300

    updates

--- 1_opcodes.c
+++ 1_opcodes.c
@@ -23,28 +23,6 @@ printf("%d\n", (*stack)->n);
 }
 
 /**
- * pop - Removes the top element of the stack
- * @stack: Double pointer to the top of the stack
- * @line_number: Line number of the opcode in the file
- */
-
-void pop(stack_t **stack, unsigned int line_number)
-{
-stack_t *temp;
-if (*stack == NULL)
-{
-fprintf(stderr, "L%u: can't pop an empty stack\n", line_number);
-free_stack(*stack);
-exit(EXIT_FAILURE);
-}
-temp = *stack;
-*stack = (*stack)->next;
-if (*stack != NULL)
-(*stack)->prev = NULL;
-free(temp);
-}
-
-/**
  * swap - Swaps the top two elements of the stack
  * @stack: Double pointer to the top of the stack
  * @line_number: Line number of the opcode in the file
