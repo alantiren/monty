abcebcc1bd6db8b6853a2dcbaae851d6ba3bbe16
alantiren <tirenalan0@gmail.com>
2023-05-24 23:38:18 +0300

    updates

--- _opcodes.c
+++ _opcodes.c
@@ -222,16 +222,18 @@ printf("%c\n", value);
 }
 
 /**
- * pstr - Prints the string starting at the top of the stack
- * @stack: Double pointer to the stack
- * @line_number: Line number of the opcode
+ * pstr - Prints the string starting at the top of the stack, followed by a newline
+ * @stack: Double pointer to the head of the stack
+ * @line_number: Line number of the opcode being executed
  */
 
-void pstr(stack_t **stack)
+void pstr(stack_t **stack, unsigned int line_number)
 {
 stack_t *current = *stack;
 
-while (current != NULL && current->n != 0 && isascii(current->n))
+(void)line_number;
+
+while (current != NULL && current->n != 0 && current->n >= 0 && current->n <= 127)
 {
 printf("%c", current->n);
 current = current->next;
