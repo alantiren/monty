6b43286bb60875e0f97ff940f158ba202088b6c4
alantiren <tirenalan0@gmail.com>
2023-05-25 01:45:31 +0300

    updates

--- free_stack.c
+++ free_stack.c
@@ -29,8 +29,10 @@ current = next;
  * @line_number: Line number of the opcode in the file
  *
  * Description: The pop function removes the top element from the stack,
- * updates the stack pointer, and frees the memory allocated for the removed element.
- * If the stack is empty, an error message is printed and the program exits with EXIT_FAILURE.
+ * updates the stack pointer,
+ * and frees the memory allocated for the removed element.
+ * If the stack is empty,
+ * an error message is printed and the program exits with EXIT_FAILURE.
  */
 void pop(stack_t **stack, unsigned int line_number)
 {
