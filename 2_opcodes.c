8a492669a285a7361a4b2e4ff234a74e0056878e
alantiren <tirenalan0@gmail.com>
2023-05-25 14:15:06 +0300

    updates

--- 2_opcodes.c
+++ 2_opcodes.c
@@ -44,15 +44,14 @@ if (*stack == NULL || (*stack)->next == NULL)
 fprintf(stderr, "L%u: can't div, stack too short\n", line_number);
 exit(EXIT_FAILURE);
 }
-
 if ((*stack)->n == 0)
 {
 fprintf(stderr, "L%u: division by zero\n", line_number);
 exit(EXIT_FAILURE);
 }
-
-(*stack)->next->n /= (*stack)->n;
+int divisor = (*stack)->n;
 pop(stack, line_number);
+(*stack)->n /= divisor;
 }
 
 /**
