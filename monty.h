e5daa45e13561bc5fa9f04a7fbbe00419260682e
alantiren <tirenalan0@gmail.com>
2023-05-25 16:43:32 +0300

    Revert "updates"
    This reverts commit f55208505a899ad15ec352c52c62fd57153bb859.

--- monty.h
+++ monty.h
@@ -9,7 +9,21 @@
 #include <ctype.h>
 
 
-
+/**
+ * struct stack_s - doubly linked list representation of a stack (or queue)
+ * @n: integer
+ * @prev: points to the previous element of the stack (or queue)
+ * @next: points to the next element of the stack (or queue)
+ *
+ * Description: doubly linked list node structure
+ * for stack, queues, LIFO, FIFO
+ */
+typedef struct stack_s
+{
+int n;
+struct stack_s *prev;
+struct stack_s *next;
+} stack_t;
 
 /**
  * struct instruction_s - opcode and its function
