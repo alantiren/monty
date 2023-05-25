b5b4ef9ce08965f7d43068c19f0e4945db3ee6d5
alantiren <tirenalan0@gmail.com>
2023-05-25 16:00:38 +0300

    updates

--- 2_opcodes.c
+++ 2_opcodes.c
@@ -74,14 +74,19 @@ free(temp);
 
 void mul(stack_t **stack, unsigned int line_number)
 {
+stack_t *temp;
+
 if (*stack == NULL || (*stack)->next == NULL)
 {
 fprintf(stderr, "L%u: can't mul, stack too short\n", line_number);
 exit(EXIT_FAILURE);
 }
 
-(*stack)->next->n *= (*stack)->n;
-pop(stack, line_number);
+temp = *stack;
+(*stack) = (*stack)->next;
+(*stack)->n = (*stack)->n * temp->n;
+(*stack)->prev = NULL;
+free(temp);
 }
 
 /**
