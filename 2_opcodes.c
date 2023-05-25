5ee8915bb9caa890eec8149a6a1cb91ccc1dda27
alantiren <tirenalan0@gmail.com>
2023-05-25 16:25:05 +0300

    updates

--- 2_opcodes.c
+++ 2_opcodes.c
@@ -133,20 +133,17 @@ free(temp);
 
 void pchar(stack_t **stack, unsigned int line_number)
 {
-int value;
-
 if (*stack == NULL)
 {
 fprintf(stderr, "L%u: can't pchar, stack empty\n", line_number);
 exit(EXIT_FAILURE);
 }
 
-value = (*stack)->n;
-if (value < 0 || value > 127)
+if ((*stack)->n < 0 || (*stack)->n > 127)
 {
 fprintf(stderr, "L%u: can't pchar, value out of range\n", line_number);
 exit(EXIT_FAILURE);
 }
 
-printf("%c\n", value);
+printf("%c\n", (*stack)->n);
 }
