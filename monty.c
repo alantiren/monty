57e1e94e5ccf6d5c8b1b0d4d01a36745193801a3
alantiren <tirenalan0@gmail.com>
2023-05-28 00:53:21 +0300

    updates

--- monty.c
+++ monty.c
@@ -91,3 +91,42 @@ if (*stack != NULL)
 *stack = new_node;
 return (new_node);
 }
+
+/**
+ * main - Entry point of the Monty interpreter.
+ * @argc: The number of command-line arguments.
+ * @argv: Array of command-line arguments.
+ *
+ * Description: The main function reads the Monty byte code file,
+ * executes the corresponding opcodes, and handles errors.
+ * It creates and initializes the stack, reads each line of the file,
+ * tokenizes the line to extract the opcode, and executes the opcode function.
+ * If an unknown opcode is encountered or an error occurs,
+ * an error message is printed and the program exits with EXIT_FAILURE.
+ *
+ * Return: EXIT_SUCCESS upon successful execution, EXIT_FAILURE otherwise.
+ */
+
+int main(int argc, char *argv[])
+{
+FILE *fp;
+
+if (argc != 2)
+{
+fprintf(stderr, "USAGE: monty file\n");
+exit(EXIT_FAILURE);
+}
+fp = fopen(argv[1], "r");
+if (fp == NULL)
+{
+fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
+exit(EXIT_FAILURE);
+}
+if (process_file(fp) == -1)
+{
+fclose(fp);
+exit(EXIT_FAILURE);
+}
+fclose(fp);
+return (EXIT_SUCCESS);
+}
--- opcode.c
+++ opcode.c
@@ -34,7 +34,6 @@ fprintf(stderr, "L%u: unknown instruction %s\n", line_number, opcode);
 return (-1);
 }
 
-int main(void)
 {
 opcode_mapping_t opcode_map[] = {
 {"push", push},
@@ -130,43 +129,4 @@ return (-1);
 free(line);
 free_stack(stack);
 return (0);
-}
-
-/**
- * main - Entry point of the Monty interpreter.
- * @argc: The number of command-line arguments.
- * @argv: Array of command-line arguments.
- *
- * Description: The main function reads the Monty byte code file,
- * executes the corresponding opcodes, and handles errors.
- * It creates and initializes the stack, reads each line of the file,
- * tokenizes the line to extract the opcode, and executes the opcode function.
- * If an unknown opcode is encountered or an error occurs,
- * an error message is printed and the program exits with EXIT_FAILURE.
- *
- * Return: EXIT_SUCCESS upon successful execution, EXIT_FAILURE otherwise.
- */
-
-int main(int argc, char *argv[])
-{
-FILE *fp;
-
-if (argc != 2)
-{
-fprintf(stderr, "USAGE: monty file\n");
-exit(EXIT_FAILURE);
-}
-fp = fopen(argv[1], "r");
-if (fp == NULL)
-{
-fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
-exit(EXIT_FAILURE);
-}
-if (process_file(fp) == -1)
-{
-fclose(fp);
-exit(EXIT_FAILURE);
-}
-fclose(fp);
-return (EXIT_SUCCESS);
 }
