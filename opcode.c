94ef8a816d725e5285809b78b3c0e8716e07e474
alantiren <tirenalan0@gmail.com>
2023-05-28 00:30:33 +0300

    updates

--- opcode.c
+++ opcode.c
@@ -14,51 +14,47 @@
  * Return: 0 if opcode executed successfully, -1 on failure
  */
 
+// Define the opcode-function mappings using an array of structures
+opcode_mapping_t opcode_map[] = {
+{"push", push},
+{"pall", pall},
+{"pint", pint},
+{"pop", pop},
+{"swap", swap},
+{"add", add},
+{"nop", nop},
+{"sub", sub},
+{"div", div_op},
+{"mul", mul},
+{"mod", mod},
+{"pchar", pchar},
+{"pstr", pstr},
+{"rotl", rotl},
+{"rotr", rotr},
+{"stack", set_mode},
+{"queue", set_mode},
+{NULL, NULL}
+};
+
 int execute_opcode(const char *opcode, stack_t **stack,
 unsigned int line_number)
 {
 if (opcode == NULL || opcode[0] == '#')
 return (0);
-if (strcmp(opcode, "push") == 0)
-push(stack, line_number);
-else if (strcmp(opcode, "pall") == 0)
-pall(stack, line_number);
-else if (strcmp(opcode, "pint") == 0)
-pint(stack, line_number);
-else if (strcmp(opcode, "pop") == 0)
-pop(stack, line_number);
-else if (strcmp(opcode, "swap") == 0)
-swap(stack, line_number);
-else if (strcmp(opcode, "add") == 0)
-add(stack, line_number);
-else if (strcmp(opcode, "nop") == 0)
-nop(stack, line_number);
-else if (strcmp(opcode, "sub") == 0)
-sub(stack, line_number);
-else if (strcmp(opcode, "div") == 0)
-div_op(stack, line_number);
-else if (strcmp(opcode, "mul") == 0)
-mul(stack, line_number);
-else if (strcmp(opcode, "mod") == 0)
-mod(stack, line_number);
-else if (strcmp(opcode, "pchar") == 0)
-pchar(stack, line_number);
-else if (strcmp(opcode, "pstr") == 0)
-pstr(stack, line_number);
-else if (strcmp(opcode, "rotl") == 0)
-rotl(stack);
-else if (strcmp(opcode, "rotr") == 0)
-rotr(stack);
-else if (strcmp(opcode, "stack") == 0)
-set_mode(stack, 1, 0);
-else if (strcmp(opcode, "queue") == 0)
-set_mode(stack, 0, 1);
-else
+int i = 0;
+while (opcode_map[i].opcode != NULL)
 {
-fprintf(stderr, "L%u: unknown instruction %s\n", line_number, opcode);
-return (-1); }
+if (strcmp(opcode, opcode_map[i].opcode) == 0)
+{
+opcode_map[i].function(stack, line_number);
 return (0);
 }
+i++;
+}
+fprintf(stderr, "L%u: unknown instruction %s\n", line_number, opcode);
+return (-1);
+}
+
 
 /**
  * set_mode - Sets the stack and queue modes.
