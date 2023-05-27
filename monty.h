45eceed99f0d5a447ffa91e0ebee1fc4ce5945a0
alantiren <tirenalan0@gmail.com>
2023-05-28 01:39:34 +0300

    updates

--- monty.h
+++ monty.h
@@ -39,6 +39,14 @@ char *opcode;
 void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
 
+/**
+ * Enum to represent the data format (stack or queue)
+ */
+typedef enum {
+    STACK,
+    QUEUE
+} data_format_t;
+
 /* Function prototypes */
 void push(stack_t **stack, unsigned int line_number);
 void pall(stack_t **stack, unsigned int line_number);
