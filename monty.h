f012e2a7196826c5ba53206ab959b32adb9aae4e
alantiren <tirenalan0@gmail.com>
2023-05-28 02:07:52 +0300

    updates

--- monty.h
+++ monty.h
@@ -41,6 +41,7 @@ void (*f)(stack_t **stack, unsigned int line_number);
 
 /**
  * Enum to represent the data format (stack or queue)
+ * @enum to represent the data format (stack or queue)
  */
 typedef enum
 {
