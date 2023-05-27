0bec53e00435b5413dbe451e1684a2d01175e630
alantiren <tirenalan0@gmail.com>
2023-05-27 23:20:02 +0300

    updates

--- free_stack.c
+++ free_stack.c
@@ -37,15 +37,17 @@ current = next;
 
 void pop(stack_t **stack, unsigned int line_number)
 {
-stack_t *temp = *stack;
+stack_t *temp;
+
 if (*stack == NULL)
 {
-fprintf(stderr, "L%u: can't pop from an empty stack\n", line_number);
+fprintf(stderr, "L%u: can't pop an empty stack\n", line_number);
 exit(EXIT_FAILURE);
 }
 
-*stack = temp->next;
-if (*stack)
+temp = *stack;
+*stack = (*stack)->next;
+if (*stack != NULL)
 (*stack)->prev = NULL;
 free(temp);
 }
