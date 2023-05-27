6738702cda5decedfd59a950e1c544f2c80a1296
alantiren <tirenalan0@gmail.com>
2023-05-28 01:07:01 +0300

    Revert "Update opcode.c"
    This reverts commit 84cb387ece959b984b0995677c62237ec2bbacdf.

--- opcode.c
+++ opcode.c
@@ -20,52 +20,12 @@ unsigned int line_number, opcode_mapping_t *opcode_map)
 {
 if (opcode == NULL || opcode[0] == '#')
 return (0);
-<<<<<<< HEAD
 int i = 0;
 while (opcode_map[i].opcode != NULL)
 {
 if (strcmp(opcode, opcode_map[i].opcode) == 0)
 {
 opcode_map[i].function(stack, line_number);
-=======
-if (strcmp(opcode, "push") == 0)
-push(stack, line_number);
-else if (strcmp(opcode, "pall") == 0)
-pall(stack, line_number);
-else if (strcmp(opcode, "pint") == 0)
-pint(stack, line_number);
-else if (strcmp(opcode, "pop") == 0)
-pop(stack, line_number);
-else if (strcmp(opcode, "swap") == 0)
-swap(stack, line_number);
-else if (strcmp(opcode, "add") == 0)
-add(stack, line_number);
-else if (strcmp(opcode, "nop") == 0)
-nop(stack, line_number);
-else if (strcmp(opcode, "sub") == 0)
-sub(stack, line_number);
-else if (strcmp(opcode, "div") == 0)
-div_op(stack, line_number);
-else if (strcmp(opcode, "mul") == 0)
-mul(stack, line_number);
-else if (strcmp(opcode, "mod") == 0)
-mod(stack, line_number);
-else if (strcmp(opcode, "pchar") == 0)
-pchar(stack, line_number);
-else if (strcmp(opcode, "pstr") == 0)
-pstr(stack, line_number);
-else if (strcmp(opcode, "rotl") == 0)
-rotl(stack);
-else if (strcmp(opcode, "rotr") == 0)
-rotr(stack);
-else if (strcmp(opcode, "stack") == 0)
-set_mode(stack, 1, 0);
-else if (strcmp(opcode, "queue") == 0)
-set_mode(stack, 0, 1);
-else{
-fprintf(stderr, "L%u: unknown instruction %s\n", line_number, opcode);
-return (-1);}
->>>>>>> parent of 6ddb1f0 (updates)
 return (0);
 }
 i++;
